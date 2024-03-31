3cd1f387e9a156ab9ec4b451cc4ac39f0513af6a
tien.nguyen23 <tien.nguyen23@mservice.com.vn>
2024-03-31 08:49:13 +0700

    add document learn swift

--- LearnSwift/HelloWorld.playground/Contents.swift
+++ LearnSwift/HelloWorld.playground/Contents.swift
@@ -1,4 +1,6 @@
 import UIKit
 
+// Document learn swift: https://docs.swift.org/swift-book/documentation/the-swift-programming-language/thebasics
+
 var greeting = "Hello, playground"
 print(greeting)
